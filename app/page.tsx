323c9018361554691b037a4a7b7873d4d79d5cb7
AbdulSubhan145 <abduls12456@gmail.com>
2024-12-12 11:10:14 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,150 @@
-import Image from "next/image";
+ import Image from "next/image";
 
-export default function Home() {
+export default function Hero() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    //hero saction
+    <section className="bg-white dark:bg-gray-900">
+    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+            <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-black hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get started
+                <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Speak to Sales
+            </a> 
+        </div>
+        <div className="hidden lg:col-span-5 lg:flex justify-center">
+    <Image 
+        src="/main pic.webp" 
+        alt="mockup" 
+        width={2900} 
+        height={290} 
+        className=" max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl" 
+    />
+</div>             
+    </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+     
+  <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
+      Raw Denim Heirloom Man Braid
+      <br className="hidden sm:block" />
+      Selfies Wayfarers
+    </h1>
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div className="p-4 md:w-1/3 flex">
+        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+        <i className="bi bi-arrow-through-heart"></i>
+        </div>
+        <div className="flex-grow pl-6">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+            Shooting Stars
+          </h2>
+          <p className="leading-relaxed text-base">
+            Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+            taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi
+            pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.
+          </p>
+          <a className="mt-3 text-indigo-500 inline-flex items-center">
+            Learn More
+            <i className="bi bi-arrow-right ml-3"></i>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="p-4 md:w-1/3 flex">
+        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+        <i className="bi bi-shop"></i>
+        </div>
+        <div className="flex-grow pl-6">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+            The Catalyzer
+          </h2>
+          <p className="leading-relaxed text-base">
+            Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+            taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi
+            pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.
+          </p>
+          <a className="mt-3 text-indigo-500 inline-flex items-center">
+            Learn More
+            <i className="bi bi-arrow-right ml-3"></i>
+          </a>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3 flex">
+        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+        <i className="bi bi-amazon"></i> 
+        </div>
+        <div className="flex-grow pl-6">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+            Neptune
+          </h2>
+          <p className="leading-relaxed text-base">
+            Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+            taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi
+            pug VHS try-hard ugh iceland kickstarter tumblr live-edge tilde.
+          </p>
+          <a className="mt-3 text-indigo-500 inline-flex items-center">
+            Learn More
+            <i className="bi bi-arrow-right ml-3 "></i>
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
+
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap w-full mb-20">
+      <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+        <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+      </div>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably havent heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+    </div>
+    <div className="flex flex-wrap -m-4">
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/mobile pic.jpg" alt="content" width={150} height={150}  />
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/mobile pic1.jpg" alt="content" width={150} height={150}  />
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/mobile pic2.jpg" alt="content" width={150} height={150}  />
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/mobile pic3.jpg" alt="content" width={150} height={150}  />
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</section>
+
+    
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+    return (
+        <header className="text-gray-600 body-font">
+       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+       
+      
+      <h1 className="ml-3 text-xl font-bold">$ & SHOP</h1>
+    </a>
+    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+      <a className="mr-5 hover:text-gray-900">First Link</a>
+      <a className="mr-5 hover:text-gray-900">Second Link</a>
+      <a className="mr-5 hover:text-gray-900">Third Link</a>
+      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+    </nav>
+   
+  </div>
+</header>
+      
+    );
+  }
